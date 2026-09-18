086284a290fb26562b11a8907dbab00e0578c297
Javier Pineda <A00845219@tec.mx>
2026-09-18 01:30:46 -0600

    Avance de como traducir las fechas a numeros

--- Evidences/Evidence1/src/ev.cpp
+++ Evidences/Evidence1/src/ev.cpp
@@ -25,3 +25,41 @@ ej. rapido, lento, estable, etc.)
 
 using namespace std;
 using namespace std::chrono;
+
+// Lo mismo que class pero todo publico por default
+struct LogEntry {
+    string mes;         // agarra el mes del log
+    string texto;       // el texto original del log
+    long long fecha;    // la fecha en formato long long para poder comparar  
+};
+
+long long convertirAFechaNumero(string mes, int dia, int hora, int min, int seg) {
+    int numMes = mesANumero(mes);
+    
+    // fecha en orden de importancia con matematicas para hacer comparaciones
+    // Año * 10000000000 + Mes * 100000000 + Dia * 1000000 + Hora * 10000 + Minuto * 100 + Segundo
+    long long fechaCompuesta = 2024LL * 10000000000LL + 
+                               numMes * 100000000LL + 
+                               dia * 1000000LL + 
+                               hora * 10000LL + 
+                               min * 100LL + 
+                               seg;
+                               
+    return fechaCompuesta;
+}
+
+int mesANumero(string mes) {
+    if (mes == "Jan") return 1;
+    if (mes == "Feb") return 2;
+    if (mes == "Mar") return 3;
+    if (mes == "Apr") return 4;
+    if (mes == "May") return 5;
+    if (mes == "Jun") return 6;
+    if (mes == "Jul") return 7;
+    if (mes == "Aug") return 8;
+    if (mes == "Sep") return 9;
+    if (mes == "Oct") return 10;
+    if (mes == "Nov") return 11;
+    if (mes == "Dec") return 12;
+    return 0;
+}
